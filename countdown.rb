1235fb6c257e94690180bb827f300ffe94871ce0
Bruce Parker <detailsharks@gmail.com>
2020-06-07 02:51:09 +0000

    Automatically backed up by Learn

--- countdown.rb
+++ countdown.rb
@@ -7,3 +7,10 @@ def countdown(number)
   "HAPPY NEW YEAR!"
 end
 
+def countdown_with_sleep(number)
+  while number > 0 do 
+  puts "#{number} SECOND(S)!"
+  sleep 1
+  number -= 1  
+  end
+end	end
